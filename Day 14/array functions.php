503b81dc1c1f66c4dcfb203977b87ad34922b04b
upasanaparekh <137034963+upasanaparekh@users.noreply.github.com>
2023-07-07 19:14:10 +0530

    Add files via upload

---
+++ Day 14/array functions.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$a=array("red","green");
+array_push($a,"blue","yellow");
+print_r($a);
+
+$a=array("a"=>"Volvo","b"=>"BMW","c"=>"Toyota");
+print_r(array_reverse($a));"<br/>"
+
+$cars=array("Volvo","BMW","Toyota");
+echo count($cars);"<br/>"
+?>
+
+</body>
+</html>
